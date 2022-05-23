56a06f8685d6514d1e39639ddd1bd3c22b082e0f
Adrian Raszka <axd088@gmail.com>
2022-05-23 23:42:08 +0200

    Added New Recipe

---
+++ food-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Pizza is a yummy wheel', 'https://cdn.galleries.smcloud.net/t/galleries/gf-cgdk-p5yy-aE4f_pizza-pepperoni-z-jalapeno-to-jadl-joe-biden-z-zolnierzami-w-rzeszowie-1920x1080-nocrop.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ food-app/src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, description: string , imagePath: string) {
+        name = this.name;
+        description = this.name;
+        imagePath = this.imagePath;
+    }
+}
